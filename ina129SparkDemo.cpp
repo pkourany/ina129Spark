c3557ee06f2014d6396e034cda69e4f1e6c0bbcb
Paul Kourany <pkourany@users.noreply.github.com>
2014-02-23 19:50:34 -0500

    Create ina129SparkDemo.cpp

---
+++ ina129SparkDemo.cpp
@@ -0,0 +1,37 @@
+// This #include statement was automatically added by the Spark IDE.
+#include "ina129Spark.h"
+
+
+Adafruit_INA219 ina219;
+
+void setup(void) 
+{
+  uint32_t currentFrequency;
+    
+  Serial.begin(115200);
+  delay(3000);                  //Delay to give a chance to start serial monitor
+
+  Serial.println("Measuring voltage and current with INA219 ...");
+  ina219.begin();
+}
+
+void loop(void) 
+{
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.println("");
+
+  delay(2000);
+}
